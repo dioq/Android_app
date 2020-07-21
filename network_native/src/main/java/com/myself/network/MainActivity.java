13299bfe26c0d41f0ccfbf4a7e2f064b68375273
Dio <zhendong2011@live.cn>
2020-07-22 04:30:21 +0800

    优化代码
    去除无用的类型强转

--- network_native/src/main/java/com/myself/network/MainActivity.java
+++ network_native/src/main/java/com/myself/network/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        show_board = (TextView) findViewById(R.id.tvId);
+        show_board = findViewById(R.id.tvId);
     }
 
     public void get_func(View view) {
