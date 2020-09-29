1a18f4662ff8defe7a280e60d8246ed0afd92941
Dio <zhendong2011@live.cn>
2020-09-29 13:18:33 +0800

    add
    添加一些新描述

--- okhttp1/src/main/java/com/myself/network/MainActivity.java
+++ okhttp1/src/main/java/com/myself/network/MainActivity.java
@@ -1,13 +1,20 @@
 package com.myself.network;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +25,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.request_text);
+        textView = findViewById(R.id.request_text);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void post_func(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String urlStr = "http://www.anant.club:8848/getPost";
+                    ArrayMap<String, Object> parmas = new ArrayMap<>();
+                    parmas.put("username", "Dio");
+                    parmas.put("password", "13131313");
+                    parmas.put("argot", "You are geat!");
+                    parmas.put("num", 1111);
+                    JSONObject param_json = new JSONObject(parmas);
+                    String json = param_json.toString();
+                    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+                    OkHttpClient client = new OkHttpClient();
+                    RequestBody body = RequestBody.create(JSON, json);
+                    Request request = new Request.Builder()
+                            .url(urlStr)
+                            .post(body)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();//处理返回的数据
+                    showResponse(responseData);//更新ui
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     public void request_func(View view) {
@@ -31,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 try {
                     OkHttpClient client = new OkHttpClient();//新建一个OKHttp的对象
-                    Request request = new Request.Builder().url("https://www.baidu.com").build();//创建一个Request对象
+                    Request request = new Request.Builder().url("http://103.100.211.187:8848/getTest").build();//创建一个Request对象
                     Response response = client.newCall(request).execute();//发送请求获取返回数据
                     String responseData = response.body().string();//处理返回的数据
                     showResponse(responseData);//更新ui
