075101c8b42e3e181f3b3fb814a0a3135823e7ec
DioSpace <zhendong2011@live.cn>
2020-12-02 01:29:05 +0800

    改名
    改几个模块名

--- proxy_dynamic/src/main/java/com/my/dynamic_proxy/MainActivity.java
+++ proxy_dynamic/src/main/java/com/my/dynamic_proxy/MainActivity.java
@@ -19,34 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "10001";
     TextView show_board;
-    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         show_board = findViewById(R.id.tvId);
-        webView = findViewById(R.id.webview_id);
-        String url_str = "http://www.ip111.cn/";
-        webView.loadUrl(url_str);
     }
 
-    public void test_func(View view) {
-        String urlStr = "http://www.ip111.cn";
-        MyOkHttp.getInstance().requestGet(urlStr, new MyOkHttp.OkHttpCallBack<String>() {
-
-            @Override
-            public void requestSuccess(String s) {
-                Log.e(TAG, "success:" + s);
-            }
-
-            @Override
-            public void requestFailure(String s) {
-                Log.e(TAG, "failure:" + s);
-            }
-        }, String.class);
-    }
-
+    //获取系统属性,查看当前代理地址
     public void get_current_proxy(View view) {
         String http_host = System.getProperty("http.proxyHost");
         String http_port = System.getProperty("http.proxyPort");
@@ -54,23 +35,24 @@ public class MainActivity extends AppCompatActivity {
         showResponse(text);
     }
 
+    //访问ip111.cn 查看网络请求时所走的 代理地址
     public void get_internet_proxy(View view) {
-        webView.reload();
         String urlStr = "http://www.ip111.cn";
         MyOkHttp.getInstance().requestGet(urlStr, new MyOkHttp.OkHttpCallBack<String>() {
 
             @Override
             public void requestSuccess(String s) {
-                Log.e(TAG, "success:" + s);
+                Log.e(TAG, "success:\n" + s);
             }
 
             @Override
             public void requestFailure(String s) {
-                Log.e(TAG, "failure:" + s);
+                Log.e(TAG, "failure:\n" + s);
             }
         }, String.class);
     }
 
+    //设置以指定代理访问网络(ip必须是有效的,否则就以本机ip访问)
     public void replace_proxy(View view) {
         System.setProperty("http.maxRedirects", "50");
         // 如果不设置，只要代理IP和代理端口正确,此项不设置也可以
@@ -81,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         System.getProperties().setProperty("http.proxyPort", port);
     }
 
-    //动态获取代理地址
+    //动态获取代理地址, 并设置成网络访问时所走的代理地址
     private void get_dynamic_proxy() {
         String urlStr = "http://route.xiongmaodaili.com/xiongmao-web/api/glip?secret=0669093de4b211ea65933ba3ca79bcbe&orderNo=GL20201126004658vSwZe7jx&count=1&isTxt=0&proxyType=1";
         MyOkHttp.getInstance().requestGet(urlStr, new MyOkHttp.OkHttpCallBack<String>() {
