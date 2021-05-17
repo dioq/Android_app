11d9dff972d1a89502317ac7241cfb4e533ed4ad
Jobs8w <job8w@outlook.com>
2021-05-17 18:40:07 +0800

    改域名
    原来的域名过期了，以后用这个新域名

--- network_native/src/main/java/com/myself/network/MainActivity.java
+++ network_native/src/main/java/com/myself/network/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {//只能在子线程中请求
             @Override
             public void run() {
-                String response = NetworkUtil.getInstance().doGet("http://www.anant.club:8081/getdata");
+                String response = NetworkUtil.getInstance().doGet("http://jobs8.cn:8081/getdata");
                 System.out.println("response:\n" + response);
                 showResponse(response);
             }
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-                String urlStr = "http://www.anant.club:8081/postdata";
+                String urlStr = "http://jobs8.cn:8081/postdata";
                 JSONObject param_json = new JSONObject();
                 try {
                     param_json.put("name", "JOJO");
@@ -80,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
             myRequetPermission();
             return;
         }
-        final String urlStr = "http://www.anant.club:8081/upload";
+        final String urlStr = "http://jobs8.cn:8081/upload";
         File dataDir = Environment.getExternalStorageDirectory();
         String path = dataDir.getPath() + File.separator + "Pictures" + File.separator;
-        final String imgPath = path + "test.jpg";
+        final String imgPath = path + "test.png";
 //        final String imgPath = new ImageUtil().getPathByImage("money.jpg");
 //        Log.e(TAG, "imgPath:\n" + imgPath);
         new Thread(new Runnable() {//只能在子线程中请求
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         final String urlStr = "https://upload.gmugmu.com/api/v1/base/resource/image/upload?source_info=eyJhcHBpZCI6IjI2MDAwMyIsInVpZCI6IjMyNDk3MjMyIiwicGFnZSI6ImNvbS5lb21jaGF0Lm1vZHVsZS5ob21lLkhvbWVQYWdlQWN0aXZpdHkiLCJ0aW1lIjoiMTYwNjI0Mjc3MDA5NiJ9&cc=TG73257&dev_name=nubia&oaid=&cpu=[Adreno_(TM)_630][ARMv7_639_placeholder]&lc=37427d9d8660d3f7&osversion=android_22&sid=02Yok7jQeFpBTR+Uz1tDaONajE3oi6XdrdqPwUyM/joBe9tJRE6grYwCweYmrwWh&ndid=&conn=wifi&ram=3650129920&msid=363635333236303436313030303634&icc=89860081133720371180&statistics=9ad290c3317d39cc8be58b98f74c86e7&mtid=e6352e4b164246b8b0be20f9c36f5abe&atid=302e30&tourist=&ongd=302e30&mtxid=00812dc6db02&evid=3535303335643264616635632d343464382d626465332d373262342d3034366638353761&cv=GM4.6.30_Android&proto=&logid=2006201,2001902,2002202,201802,202101,204005,204401,2005402,2006102,2001701,2003301,2005601,2005502,2001501,2005202,204301,2001802,2005801,2003502,2004701,2000802,201007,2001602,2004901,2002101,2003802,2005902,204101,2006001,2004801,2001202&ua=nubiaNX629J&uid=32497232&vv=202010141815&meid=363834373537373230363631353638&smid=&aid=b35517406764bf51";
         File dataDir = Environment.getExternalStorageDirectory();
         String path = dataDir.getPath() + File.separator + "Pictures" + File.separator;
-        final String imgPath = path + "test.jpg";
+        final String imgPath = path + "test.png";
 //        Log.e(TAG, "imgPath:\n" + imgPath);
         new Thread(new Runnable() {//只能在子线程中请求
             @Override
@@ -118,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-                String urlStr = "http://www.anant.club:8081/formdata";//接口地址
+                String urlStr = "http://jobs8.cn:8081/formdata";//接口地址
                 HashMap<String, String> params = new HashMap<>();
                 params.put("name", "Dio");
                 params.put("age", "21");
@@ -135,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 SSLConfig.set(SSLTrustWhich.TrustAll);
 
-                String urlStr = "https://www.anant.club:8082/getdata";
+                String urlStr = "https://jobs8.cn:8082/getdata";
                 String response = NetworkUtil.getInstance().doGet(urlStr);
                 System.out.println("get_tls_func response:\n" + response);
                 showResponse(response);
@@ -150,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 SSLConfig.set(SSLTrustWhich.TrustMeOneway);
 
-                String urlStr = "https://www.anant.club:8082/postdata";
+                String urlStr = "https://jobs8.cn:8082/postdata";
                 JSONObject param_json = new JSONObject();
                 try {
                     param_json.put("name", "JOJO");
@@ -172,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 SSLConfig.set(SSLTrustWhich.TrustMeTwoway);
 
-                String urlStr = "https://www.anant.club:8083/getdata";
+                String urlStr = "https://jobs8.cn:8083/getdata";
                 String response = NetworkUtil.getInstance().doGet(urlStr);
                 System.out.println("get_tls_twoway_func response:\n" + response);
                 showResponse(response);
